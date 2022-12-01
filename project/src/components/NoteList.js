cc360f5bf33cb3f6ad701e28adc83ff2df288526
Niko Kallio <niko.kallio99@outlook.com>
2022-12-01 03:46:24 +0200

    update working with hardcoded changes

--- project/src/components/NoteList.js
+++ project/src/components/NoteList.js
@@ -15,16 +15,6 @@ const NoteList = (props) => {
     http.delete('https://projekti-50a74-default-rtdb.europe-west1.firebasedatabase.app/notes/'+ key + '.json')
     
   }
-/*
-  const updateHandler = (key) =>{
-    
-    console.log("clicked! update " + key);
-    updateNote.update('https://projekti-50a74-default-rtdb.europe-west1.firebasedatabase.app/notes/'+ key + '.json')
-    
-  }
-   
-*/
-
 
 const updateHandler = async (items) => {
   console.log(items);
