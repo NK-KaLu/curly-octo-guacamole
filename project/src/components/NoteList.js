c38291b8eef414f94bccf8f379914e036cd69211
Niko Kallio <niko.kallio99@outlook.com>
2022-12-02 02:58:39 +0200

    Update working

--- project/src/components/NoteList.js
+++ project/src/components/NoteList.js
@@ -1,8 +1,10 @@
 import DeleteHTTP from "./DeleteNote";
+
+
 const NoteList = (props) => {
 
 
-  const http = new DeleteHTTP;
+  const http = new DeleteHTTP();
   
   const deleteHandler = (key) =>{
     
@@ -13,12 +15,15 @@ const NoteList = (props) => {
 
 const updateHandler = async (items) => {
   console.log(items);
+  let person = prompt("Enter your changes", items.name);
+  let date = prompt("Change date", items.date);
+  if(person != null && date != null){
   const response = await fetch(
     "https://projekti-50a74-default-rtdb.europe-west1.firebasedatabase.app/notes/" + items.id +".json",
     {
       method: "PUT",
-      body: JSON.stringify({name:"tepsu on best",
-      date: "4-20-69"}),
+      body: JSON.stringify({name:person,
+      date: date}),
       headers: {
         "Content-Type": "application/json",
       },
@@ -27,18 +32,17 @@ const updateHandler = async (items) => {
   const data = await response.json();
   console.log(data);
   window.location.reload()
+  };
 };
   
-
-
-   
-
-    return (
+  return (
       <ul className="items-list">
         {props.items.map((items) => (
           
           <div>
-            <p>{items.date} {items.name} <button onClick={() => deleteHandler(items.id)}>Delete</button>  <button onClick={() => updateHandler(items)}>Update</button></p>
+            <p>
+              {items.date} {items.name} <button onClick={() => deleteHandler(items.id)}>Delete</button>  <button onClick={() => updateHandler(items)}>Update</button>
+            </p>
           </div>
         ))}
       </ul>
